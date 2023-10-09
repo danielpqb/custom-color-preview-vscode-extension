d030c2397bb2f90f98d436e0ae09b8e33d47197b
danielpqb <danielpqb@gmail.com>
2023-10-09 02:37:49 -0300

    v0.0.6

--- src/extension.ts
+++ src/extension.ts
@@ -1,29 +1,35 @@
 import { Picker } from "./helper";
 import { CONFIG_GLOB } from "./lib/constants";
-import { ExtensionContext, workspace as Workspace, Uri } from "vscode";
+import {
+  ExtensionContext,
+  workspace as Workspace,
+  Uri,
+} from "vscode";
 
 // This method is called when your extension is activated
 export async function activate(context: ExtensionContext) {
-  const path = await Workspace.findFiles(
-    CONFIG_GLOB,
-    "**/{node_modules, out, dist}/**",
-    1
-  ).then((doc) => {
-    return doc[0].path;
-  });
-
-  const uri = Uri.parse(path);
-
-  const customColors = await Workspace.openTextDocument(uri).then((obj) => {
-    return JSON.parse(obj.getText());
-  });
-
-  const picker = new Picker(customColors);
-  context.subscriptions.push(picker);
-
-  console.log(
-    'Congratulations, your extension "custom-color-preview-vscode-extension" is now active!'
-  );
+  setTimeout(async () => {
+    const path = await Workspace.findFiles(
+      CONFIG_GLOB,
+      "**/{node_modules, out, dist}/**",
+      1
+    ).then((doc) => {
+      return doc[0].path;
+    });
+  
+    const uri = Uri.parse(path);
+  
+    const customColors = await Workspace.openTextDocument(uri).then((obj) => {
+      return JSON.parse(obj.getText());
+    });
+  
+    const picker = new Picker(customColors);
+    context.subscriptions.push(picker);
+  
+    console.log(
+      'Congratulations, your extension "custom-color-preview-vscode-extension" is now active!'
+    );
+  }, 2000);
 }
 
 // This method is called when your extension is deactivated
